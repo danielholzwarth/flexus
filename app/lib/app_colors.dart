a4f027d190e573a54e0f8a9d11d9b93768431110
danielholzwarth <danielholzwarth@gmx.net>
2024-02-07 12:00:30 +0100

    feat: add AppColors

---
+++ app/lib/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/services.dart';
+
+class AppColors {
+  static var primary = const Color.fromRGBO(0, 116, 182, 100);
+  static var primaryV1 = const Color.fromRGBO(0, 116, 182, 30);
+  static var primaryV2 = const Color.fromRGBO(0, 116, 182, 20);
+
+  static var background = const Color.fromARGB(255, 255, 255, 255);
+
+  static var error = const Color.fromRGBO(176, 0, 32, 100);
+
+  static var font = const Color.fromRGBO(0, 0, 0, 100);
+  static var blocked = const Color.fromRGBO(0, 0, 0, 20);
+}
