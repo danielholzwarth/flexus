757862e4c7099bf4bde998c5196f4943113433d0
danielholzwarth <danielholzwarth@gmx.net>
2024-04-20 00:45:37 +0200

    feat: Get only notifications from gyms where you joined

--- backend/internal/pkg/storage/postgres/notification.go
+++ backend/internal/pkg/storage/postgres/notification.go
@@ -10,12 +10,21 @@ func (db *DB) GetNewUsersWorkingOut(userAccountID int) ([]types.NotificationNewU
 	query := `
 		SELECT u.name, w.starttime, COALESCE(g.name, 'null') AS gym_name
 		FROM user_account AS u
-		INNER JOIN friendship AS f ON (u.id = f.requestor_id AND $1 = f.requested_id OR $1 = f.requestor_id AND u.id = f.requested_id)
-		INNER JOIN workout AS w ON u.id = w.user_id
-		LEFT JOIN gym AS g ON g.id = w.gym_id
-		WHERE u.id != $1 
+		JOIN friendship AS f ON u.id = f.requestor_id OR u.id = f.requested_id
+		JOIN workout AS w ON u.id = w.user_id
+		JOIN gym AS g ON g.id = w.gym_id
+		WHERE u.id != $1
+		AND ($1 = f.requested_id OR $1 = f.requestor_id)
 		AND f.is_accepted = TRUE 
-		AND w.created_at > NOW() - INTERVAL '30 seconds' AND w.created_at < NOW();
+		AND w.created_at > NOW() - INTERVAL '30 seconds' 
+		AND w.created_at < NOW()
+		AND EXISTS (
+			SELECT 1 
+			FROM user_account_gym AS uag
+			JOIN user_account u2 ON u2.id = uag.user_id
+			WHERE u2.id = $1
+			AND uag.gym_id = w.gym_id
+		);
     `
 
 	rows, err := db.pool.Query(query, userAccountID)
